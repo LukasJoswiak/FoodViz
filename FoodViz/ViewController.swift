7acdd27f9492e415e7a8a131446ab3465216a765
Lukas Joswiak <ljoswiak@cs.washington.edu>
2017-10-21 17:19:09 -0700

    Initial Commit

---
+++ FoodViz/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  FoodViz
+//
+//  Created by Lukas Joswiak on 10/21/17.
+//  Copyright © 2017 Lukas Joswiak. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
